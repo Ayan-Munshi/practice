b35d554b52514a565d57a81c81eb612568713551
ayan munshi <ayanmunshi@ayans-MacBook-Air.local>
2024-01-20 17:13:16 +0530

    updated

--- bmi-calculator-practice/bmi.js
+++ bmi-calculator-practice/bmi.js
@@ -6,6 +6,7 @@ button.addEventListener("click", function (e) {
   const height = parseInt(document.querySelector(".height").value); // have to parse the value in int because by default we get string value we cant calculate with string
   const weight = parseInt(document.querySelector(".weight").value); // .value gives us the value of the input field selected by the queryselector()
   const result = document.querySelector("#results");
+  const conclusion = document.querySelector('#conclusion')
 
   //     let a = parseInt(height.value)
   //     let b = parseInt(weight.value)
@@ -20,6 +21,25 @@ button.addEventListener("click", function (e) {
     result.style.color = 'red'
   }
   else{
-    result.innerHTML= (`Your BMI is : ${(weight/((height*height)/10000)).toFixed(3)}`) // 3 numbers after decimal
+     
+    const final_calculation = (weight/((height*height)/10000)).toFixed(3)
+
+    result.innerHTML= (`Your BMI is : ${final_calculation}`) // 3 numbers after decimal
+
+    if(final_calculation <  18.6 ){
+      
+      conclusion.innerHTML = 'under eight'
+      conclusion.style.color = 'red'
+     }
+    else if(final_calculation > 24.9){
+      conclusion.innerHTML = 'over weight'
+      conclusion.style.color = 'red'
+     }
+    else{
+      conclusion.innerHTML = 'normal weight'
+      conclusion.style.color = 'green'
+    }
+      
+
   }
 });
